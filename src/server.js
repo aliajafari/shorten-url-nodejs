29ec3d0ecb84a837542ca65d0606ecad05e5f8c1
Ali A. Jafari <ali.jafari@zoodfood.com>
2022-06-12 13:23:53 +0430

    remove duplicate line

--- src/server.js
+++ src/server.js
@@ -13,7 +13,6 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')))
-app.use(express.static(path.join(__dirname, 'public')))
 app.use(bodyParser.json());
 
 MongoClient.connect(databaseUrl, { useNewUrlParser: true })
